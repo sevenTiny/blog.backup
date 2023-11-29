1d7511c50f891450bcaf1903cd4436f330761229
sevenTiny <seventiny@foxmail.com>
2023-11-29 18:34:26 +0800

    add set more tool and use tool adjustment post

--- tools/set_more.py
+++ tools/set_more.py
@@ -6,5 +6,29 @@ for filename in os.listdir(path):
     # read file content
     with open(os.path.join(path, filename), 'r+', encoding='UTF-8') as f:
         content = f.read()
+
+        # get content length
+        length = len(content)
+
+        if (length < 500):
+            continue
+
+        # if <!-- more --> exists, then skip
         if (content.find('<!-- more -->') != -1):
-            print('find more in ' + filename)
+            continue
+
+        # find the first \n after 300
+        index = 300
+        while (content[index] != '\n'):
+            index += 1
+
+        # if the first \n is after 300, then add <!-- more -->
+        if (index > 300 and index < length-1):
+            content = content[:index] + '\n\n<!-- more -->' + content[index:]
+            print(filename + ' added <!-- more --> at ' + str(index))
+            # print(content)
+
+        # write file
+        f.seek(0)
+        f.truncate()
+        f.write(content)
